c478a959af415753dddecb4c9add89ab9a3071e8
ijbs-dev <114712275+ijbs-dev@users.noreply.github.com>
2023-08-09 01:58:53 -0300

    Update App-Git.py

--- App-Git.py
+++ App-Git.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import streamlit as st
-import plotly.express as px
+import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestRegressor
 
 # Funcao para carregar o dataset
@@ -17,73 +17,77 @@ def train_model():
     rf_regressor.fit(x, y)
     return rf_regressor
 
-# Criando um dataframe
-data = get_data()
+def main():
+    # Criando um dataframe
+    data = get_data()
 
-# Treinando o modelo
-model = train_model()
-data
+    # Treinando o modelo
+    model = train_model()
 
-# Titulo
-st.title("Data App - Prevendo Valores de Imoveis")
+    # Titulo
+    st.title("Data App - Prevendo Valores de Imoveis")
 
-# Subtitulo
-st.markdown("Este é um Data App utilizado para exibir a solição de Machine Learning para o problema de predição de valores de imoveis em Boston.")
+    # Subtitulo
+    st.markdown("Este é um Data App utilizado para exibir a solução de Machine Learning para o problema de predição de valores de imóveis em Boston.")
 
-# Verificando o dataset
-st.subheader("Selecionando apenas um pequeno conjunto de atributos")
+    # Verificando o dataset
+    st.subheader("Selecionando apenas um pequeno conjunto de atributos")
 
-# Atributos para serem exibidos por padrão
-defaultcols = ["RM", "PTRATIO", "LSTAT", "MEDV"]
+    # Atributos para serem exibidos por padrão
+    defaultcols = ["RM", "PTRATIO", "LSTAT", "MEDV"]
 
-# Obtendo a lista de opções
-options = data.columns.tolist()
+    # Obtendo a lista de opções
+    options = data.columns.tolist()
 
-# Filtrando os valores padrão que estão presentes na lista de opções
-defaultcols_filtered = [col for col in defaultcols if col in options]
+    # Filtrando os valores padrão que estão presentes na lista de opções
+    defaultcols_filtered = [col for col in defaultcols if col in options]
 
-# Definindo atributos a partir do multiselect
-cols = st.multiselect("Atributos", options, default=defaultcols_filtered)
+    # Definindo atributos a partir do multiselect
+    cols = st.multiselect("Atributos", options, default=defaultcols_filtered)
 
-# Exibindo os top 10 registros do dataframe
-st.dataframe(data[cols].head(10))
+    # Exibindo os top 10 registros do dataframe
+    st.dataframe(data[cols].head(10))
 
-st.subheader("Distribuição de Imoveis por preço")
+    st.subheader("Distribuição de Imoveis por preço")
 
-# Definindo a faixa de valores
-faixa_valores = st.slider("Faixa de preço", float(data.MEDV.min()), 150., (10.0, 100.0))
+    # Definindo a faixa de valores
+    faixa_valores = st.slider("Faixa de preço", float(data.MEDV.min()), 150., (10.0, 100.0))
 
-# Filtrando os dados
-dados = data[data['MEDV'].between(left=faixa_valores[0], right=faixa_valores[1])]
+    # Filtrando os dados
+    dados = data[data['MEDV'].between(left=faixa_valores[0], right=faixa_valores[1])]
 
-# Plot a distribuição dos dados
-f = px.histogram(dados, x="MEDV", nbins=100, title="Distribuição de Preços")
-f.update_xaxes(title="MEDV")
-f.update_yaxes(title="Total Imóveis")
-st.plotly_chart(f)
+    # Plot a distribuição dos dados usando Matplotlib
+    st.subheader("Distribuição de Preços")
+    plt.hist(dados['MEDV'], bins=100)
+    plt.xlabel("MEDV")
+    plt.ylabel("Total Imóveis")
+    st.pyplot()  # Exibe o gráfico Matplotlib
 
-st.sidebar.subheader("Defina os atributos do imovel para predição")
+    st.sidebar.subheader("Defina os atributos do imovel para predição")
 
-# Mapeando dados do usuario para cada atributo
-crim = st.sidebar.number_input("Taxa de Criminalidade", value=data.CRIM.mean())
-indus = st.sidebar.number_input("Proporção de Hectares de Negócio", value=data.INDUS.mean())
-chas = st.sidebar.selectbox("Faz limite com o rio?", ("Sim", "Não"))
+    # Mapeando dados do usuario para cada atributo
+    crim = st.sidebar.number_input("Taxa de Criminalidade", value=data.CRIM.mean())
+    indus = st.sidebar.number_input("Proporção de Hectares de Negócio", value=data.INDUS.mean())
+    chas = st.sidebar.selectbox("Faz limite com o rio?", ("Sim", "Não"))
 
-# Transformando o dado de entrada em valor binario
-chas = 1 if chas == "Sim" else 0
+    # Transformando o dado de entrada em valor binario
+    chas = 1 if chas == "Sim" else 0
 
-nox = st.sidebar.number_input("Concentração de Oxido Nitrico", value=data.NOX.mean())
+    nox = st.sidebar.number_input("Concentração de Oxido Nitrico", value=data.NOX.mean())
 
-rm = st.sidebar.number_input("Numero de Quartos", value=1)
+    rm = st.sidebar.number_input("Numero de Quartos", value=1)
 
-ptratio = st.sidebar.number_input("Indice de alunos para professores", value=data.PTRATIO.mean())
+    ptratio = st.sidebar.number_input("Indice de alunos para professores", value=data.PTRATIO.mean())
 
-# Inserindo um botao na tela
-btn_predict = st.sidebar.button("Realizar Predição")
+    # Inserindo um botao na tela
+    btn_predict = st.sidebar.button("Realizar Predição")
 
-# Verifica se o botão foi acionado
-if btn_predict:
-    result = model.predict([[crim, indus, chas, nox, rm, ptratio]])
-    st.sidebar.subheader("O valor previsto para o imóvel é:")
-    result = "US $ " + str(round(result[0]*10, 2))
-    st.sidebar.write(result)
+    # Verifica se o botão foi acionado
+    if btn_predict:
+        result = model.predict([[crim, indus, chas, nox, rm, ptratio]])
+        st.sidebar.subheader("O valor previsto para o imóvel é:")
+        result = "US $ " + str(round(result[0]*10, 2))
+        st.sidebar.write(result)
+
+if __name__ == "__main__":
+    main()
